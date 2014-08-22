b6a93359421f8356355ea1e38309bece12fa6d4d
air <251949141@qq.com>
2014-08-22 12:11:54 +0800

    edit
    edit

--- assets/amr_encode.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# Usage: encoder wav_file amr_output
-# .sw is a sox shorthand for signed word .raw
-# Hope that sox will create correct endian.
-sox "$1" -r 8000 "$1".sw resample
-amrnb-encoder MR515 "$1".sw "$2"
-rm "$1".sw
--- assets/se_t630.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-amrnb-decoder se_t630.amr se_t630.raw
-aplay -t raw -c 1 -r 8000 -f S16_LE se_t630.raw
